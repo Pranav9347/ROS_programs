c4098aa9f3a3a92aff0ccb315d1f10d7350be35b
Pranav9347 <112823461+Pranav9347@users.noreply.github.com>
2023-05-18 23:47:16 +0530

    Update chat.cpp
    tried to pass a class, but I didn't know how to continue

--- auv_pkg2/src/chat.cpp
+++ auv_pkg2/src/chat.cpp
@@ -8,28 +8,33 @@
 #include<string>
 using namespace std;
 
-void cb(const auv_pkg::custom::ConstPtr& ptr)
-{
-    cout<<(ptr->name.c_str())<<endl;
-}
-
 int main(int argc, char **argv)
 {
+    class x
+   {
+        public:
+        void cb(const std_msgs::String::ConstPtr& ptr)
+        {
+            cout<<(ptr->data.c_str())<<endl;
+        }
+        void unsub(void){
+    sub.shutdown();
+        }
+   };
     string name;
     cout<<"Enter node name: ";
     cin>>name;
 ros::init(argc,argv,name);
 ros::NodeHandle nh;
 ros::Publisher pub = nh.advertise<auv_pkg::custom>("tpc",1000);
+     x var;
+ros::Subscriber sub = nr.subscribe("chatter", 1000, &x::cb, &var);
 //ros::Subscriber sub = nh.subscribe("data",1000,cb);
 ros::Rate loop_rate(10);
 cout<<"Node has been initialized"<<endl;
 
 while(ros::ok())
 {
-    int i=0;
-    if(i==0)
-    ros::Subscriber sub = nh.subscribe("data",1000,cb);
     auv_pkg::custom msg;
 string message;
 cout<<"Ready to take input\n";
@@ -42,7 +47,6 @@ ros::spin();
 //sub = nh.subscribe("tpc",1000,cb);
 ros::spinOnce();
 loop_rate.sleep();
-    i++;
 }
 
 return 0;
